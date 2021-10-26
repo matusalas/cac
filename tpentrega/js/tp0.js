03bdac999a6575bf06028aa4399e1b2f66e19bef
matusalas <93172176+matusalas@users.noreply.github.com>
2021-10-25 23:43:51 -0300

    Add files via upload

---
+++ tpentrega/js/tp0.js
@@ -0,0 +1,13 @@
+               //Nombre      dni     contraseña     mail
+const form = ["Mateo", "12345678", "contra123", "mateo@asd.com"];
+console.log(form[0]);
+console.log(form[1]);
+var asd= "Mateo";
+function accionar(){
+    if(asd == form[0]){  //&& form[1] == document.getElementById("pass")
+        console.log("esta bien");
+    }else
+    console.log("mal");
+}
+
+// document.getElementById(username)
